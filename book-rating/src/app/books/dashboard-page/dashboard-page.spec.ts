007e3a02244383d5153a3544cef2e18d3c7451c6
Ferdinand Malcher <ferdinand@malcher.media>
2025-11-06 13:19:52 +0100

    Test: Mock für BookRatingHelper

--- book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
+++ book-rating/src/app/books/dashboard-page/dashboard-page.spec.ts
@@ -2,15 +2,29 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardPage } from './dashboard-page';
 import { provideZonelessChangeDetection } from '@angular/core';
+import { Book } from '../shared/book';
+import { BookRatingHelper } from '../shared/book-rating-helper';
 
 describe('DashboardPage', () => {
   let component: DashboardPage;
   let fixture: ComponentFixture<DashboardPage>;
 
   beforeEach(async () => {
+
+    const ratingHelperMock = {
+      rateUp: (b: Book) => b,
+      rateDown: (b: Book) => b,
+    };
+
     await TestBed.configureTestingModule({
       imports: [DashboardPage],
-      providers: [provideZonelessChangeDetection()]
+      providers: [
+        provideZonelessChangeDetection(),
+        // RatingHelper ersetzen:
+        // Immer wenn jemand BRH anfordert, wird stattdessen
+        // unser ratingHelperMock ausgeliefert
+        { provide: BookRatingHelper, useValue: ratingHelperMock }
+      ]
     })
     .compileComponents();
 
