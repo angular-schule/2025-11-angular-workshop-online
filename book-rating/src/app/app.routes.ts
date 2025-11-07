9dcf5c24fd53bb8bcb86647205feeed8d4685718
Ferdinand Malcher <ferdinand@malcher.media>
2025-11-07 11:01:26 +0100

    Routing: redirectTo, Wildcard

--- book-rating/src/app/app.routes.ts
+++ book-rating/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { booksRoutes } from './books/books.routes';
+import { NotfoundPage } from './notfound-page/notfound-page';
 
 export const routes: Routes = [
-  ...booksRoutes
+  // bei Weiterleitung vom leeren Pfad ist (fast) immer pathMatch:full nötig
+  { path: '', redirectTo: 'books', pathMatch: 'full' },
+  ...booksRoutes,
+  // Wildcard-Route, muss ganz unten stehen!
+  { path: '**', component: NotfoundPage },
 ];
