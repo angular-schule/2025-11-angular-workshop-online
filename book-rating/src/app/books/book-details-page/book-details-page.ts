821ed24f81e0cf5c6c19f6a3bb1b09a620d4f3ca
Ferdinand Malcher <ferdinand@malcher.media>
2025-11-07 12:26:25 +0100

    Routing: Detailseite

--- book-rating/src/app/books/book-details-page/book-details-page.ts
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { BookStore } from '../shared/book-store';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'app-book-details-page',
@@ -8,5 +10,33 @@ import { RouterLink } from '@angular/router';
   styleUrl: './book-details-page.scss'
 })
 export class BookDetailsPage {
+  #route = inject(ActivatedRoute);
+  #store = inject(BookStore);
 
+  protected readonly book = signal<Book | undefined>(undefined);
+
+  constructor() {
+    // Pull
+    // const isbn = this.#route.snapshot.paramMap.get('isbn');
+
+    // Push
+    // TODO. Verschachtelte Subscriptions vermeiden
+    this.#route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn');
+      if (isbn) {
+        this.#store.getSingle(isbn).subscribe(book => {
+          this.book.set(book);
+        })
+      }
+    });
+
+  }
 }
+
+
+/*
+TODO
+- ISBN aus der URL lesen
+- Buch laden per HTTP
+- Buch anzeigen
+*/
