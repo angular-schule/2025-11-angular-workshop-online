3bc3a15a930731b3ca20dd75292acf33dbb524c3
Ferdinand Malcher <ferdinand@malcher.media>
2025-11-10 11:48:57 +0100

    Reactive Forms: Datenbindung

--- book-rating/src/app/books/book-create-page/book-create-page.ts
+++ book-rating/src/app/books/book-create-page/book-create-page.ts
@@ -1,11 +1,34 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './book-create-page.html',
   styleUrl: './book-create-page.scss'
 })
 export class BookCreatePage {
-
+  protected readonly bookForm = new FormGroup({
+    isbn: new FormControl('', {
+      nonNullable: true,
+      validators: []
+    }),
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: []
+    }),
+    description: new FormControl('', {
+      nonNullable: true,
+      validators: []
+    }),
+    price: new FormControl(0, {
+      nonNullable: true,
+      validators: []
+    }),
+    rating: new FormControl(1, {
+      nonNullable: true,
+      validators: []
+    }),
+  });
 }
