0233f856344cab2dbce35fcdd07885b3493cb05c
Ferdinand Malcher <ferdinand@malcher.media>
2025-11-10 12:26:24 +0100

    Reactive Forms: Fehler anzeigen

--- book-rating/src/app/books/book-create-page/book-create-page.ts
+++ book-rating/src/app/books/book-create-page/book-create-page.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-book-create-page',
@@ -12,11 +12,19 @@ export class BookCreatePage {
   protected readonly bookForm = new FormGroup({
     isbn: new FormControl('', {
       nonNullable: true,
-      validators: []
+      validators: [
+        Validators.required,
+        Validators.minLength(13),
+        Validators.maxLength(13),
+        Validators.pattern(/^[0-9]*$/)
+      ]
     }),
     title: new FormControl('', {
       nonNullable: true,
-      validators: []
+      validators: [
+        Validators.required,
+        Validators.maxLength(80)
+      ]
     }),
     description: new FormControl('', {
       nonNullable: true,
@@ -24,11 +32,47 @@ export class BookCreatePage {
     }),
     price: new FormControl(0, {
       nonNullable: true,
-      validators: []
+      validators: [
+        Validators.required,
+        Validators.min(0)
+      ]
     }),
     rating: new FormControl(1, {
       nonNullable: true,
-      validators: []
+      validators: [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(5),
+      ]
     }),
   });
+
+  isInvalid(control: FormControl): boolean | null {
+    if (control.untouched) {
+      return null;
+    }
+    return control.invalid;
+  }
+
+  hasError(control: FormControl, errorCode: string): boolean | null {
+    if (control.untouched) {
+      return null;
+    }
+
+    // return control.errors[errorCode];
+    return control.hasError(errorCode);
+  }
 }
+
+/*
+TODO:
+- Fehlermeldungen anzeigen
+  - "Die ISBN ist ungültig"
+  - "Die ISBN ist zu kurz"
+- Submit verhindern, wenn ungültig
+- Autoren erfassen
+- Formular abschicken
+  - zum Server schicken (POST)
+  - wegnavigieren zur Detailseite
+- (ISBN auf Existenz prüfen)
+*/
