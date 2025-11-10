f47b908b960e0a1228300b52c1c55ecff55a8ce3
Ferdinand Malcher <ferdinand@malcher.media>
2025-11-10 11:03:25 +0100

    Nested Routes

---
+++ book-rating/src/app/books/books-entry-page/books-entry-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BooksEntryPage } from './books-entry-page';
+
+describe('BooksEntryPage', () => {
+  let component: BooksEntryPage;
+  let fixture: ComponentFixture<BooksEntryPage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BooksEntryPage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BooksEntryPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- book-rating/src/app/books/books.routes.ts
+++ book-rating/src/app/books/books.routes.ts
@@ -4,11 +4,20 @@ import { BookDetailsPage } from "./book-details-page/book-details-page";
 import { BookDetailsResourcePage } from "./book-details-resource-page/book-details-resource-page";
 import { BookCreatePage } from "./book-create-page/book-create-page";
 import { BookSearchPage } from "./book-search-page/book-search-page";
+import { BooksEntryPage } from "./books-entry-page/books-entry-page";
 
 export const booksRoutes: Routes = [
-  { path: '', component: DashboardPage, title: 'Dashboard' },
-  { path: 'create', component: BookCreatePage, title: 'Buch erstellen' },
-  { path: 'search', component: BookSearchPage, title: 'Suche' },
-  { path: ':isbn', component: BookDetailsPage, title: 'Details' },
-  { path: 'details/:isbn', component: BookDetailsResourcePage, title: 'Details' },
+  {
+    path: '',
+    component: BooksEntryPage,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardPage, title: 'Dashboard' },
+      { path: 'create', component: BookCreatePage, title: 'Buch erstellen' },
+      { path: 'search', component: BookSearchPage, title: 'Suche' },
+      { path: ':isbn', component: BookDetailsPage, title: 'Details' },
+      { path: 'details/:isbn', component: BookDetailsResourcePage, title: 'Details' },
+    ]
+  },
+
 ];
