9600a7cc126fb812fe45ff74ff29b6126d7ec371
Ferdinand Malcher <ferdinand@malcher.media>
2025-11-11 08:17:03 +0100

    Hausaufgabe: Datum anzeigen und beenden

--- book-rating/src/app/home-page/home-page.ts
+++ book-rating/src/app/home-page/home-page.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnDestroy, signal } from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
 
+  protected readonly d = signal(Date.now());
+  #interval = setInterval(() => {
+    this.d.set(Date.now());
+    console.log(this.d())
+  }, 1000);
+
+  ngOnDestroy() {
+    clearInterval(this.#interval)
+  }
 }
