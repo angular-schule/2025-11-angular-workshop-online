20ef2799e8669ecb581cf61520d0c0a74a7be3c6
Ferdinand Malcher <ferdinand@malcher.media>
2025-11-12 11:22:41 +0100

    Detailseite mit toObservable für Routenparameter

--- book-rating/src/app/books/book-details-page/book-details-page.ts
+++ book-rating/src/app/books/book-details-page/book-details-page.ts
@@ -1,9 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { BookStore } from '../shared/book-store';
 import { Book } from '../shared/book';
 import { filter, map, switchMap } from 'rxjs';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-book-details-page',
@@ -12,14 +12,21 @@ import { toSignal } from '@angular/core/rxjs-interop';
   styleUrl: './book-details-page.scss'
 })
 export class BookDetailsPage {
-  #route = inject(ActivatedRoute);
+  // #route = inject(ActivatedRoute);
   #store = inject(BookStore);
+  readonly isbn = input.required<string>(); // Component Input Binding
 
-  protected readonly book = toSignal(this.#route.paramMap.pipe(
+  /*protected readonly book = toSignal(this.#route.paramMap.pipe(
     map(params => params.get('isbn')),
     filter(isbn => isbn !== null),
     switchMap(isbn => this.#store.getSingle(isbn)),
-  ));
+  ));*/
+
+  protected readonly book = toSignal(
+    toObservable(this.isbn).pipe(
+      switchMap(isbn => this.#store.getSingle(isbn))
+    )
+  );
 }
 
 
