453889b6b0d78374aba8e021019ae8d5283f9f34
Ferdinand Malcher <ferdinand@malcher.media>
2025-11-12 15:30:42 +0100

    LÖSUNG RxJS Dragdrop

--- rxjs-playground/src/app/exercise-dragdrop/exercise-dragdrop.ts
+++ rxjs-playground/src/app/exercise-dragdrop/exercise-dragdrop.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, signal, viewChild } from '@angular/core';
 import { toObservable } from '@angular/core/rxjs-interop';
-import { fromEvent, concatMap, first } from 'rxjs';
+import { fromEvent, concatMap, first, takeUntil } from 'rxjs';
 
 @Component({
   templateUrl: './exercise-dragdrop.html',
@@ -34,6 +34,11 @@ export class ExerciseDragdrop {
 
     /******************************/
 
+    this.mouseDown$.pipe(
+      concatMap(() => this.mouseMove$.pipe(takeUntil(this.mouseUp$)))
+    ).subscribe(e => {
+      this.setTargetPosition(e);
+    });
 
     /******************************/
   }
